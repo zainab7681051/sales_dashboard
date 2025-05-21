97db5019c79e69a128ed62ef91d20566e5dbd3ba
zainab7681051 <zainab76810510@gmail.com>
2025-05-21 18:28:23 +0300

    commit

--- src/router.js
+++ src/router.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import home from "./views/dashboardView.vue"
 
 const routes = [
-  { path: "/", name: "dashboard", component: () => import("./views/dashboardView.vue") },
+  { path: "/", name: "dashboard", component: home },
   { path: "/leaderboard", name: "leaderboard", component: () => import("./views/leaderboardView.vue") },
   { path: "/order", name: "order", component: () => import("./views/orderView.vue") },
   { path: "/product", name: "products", component: () => import("./views/productView.vue") },
@@ -9,6 +10,11 @@ const routes = [
   { path: "/messages", name: "messages", component: () => import("./views/messagesView.vue") },
   { path: "/settings", name: "settings", component: () => import("./views/settingsView.vue") },
   { path: "/signout", name: "sign out", component: () => import("./views/signoutView.vue") },
+  // catch-all fallback route
+  {
+    path: "/:pathMatch(.*)*",
+    component: home,
+  },
 ]
 
 const router = createRouter({
